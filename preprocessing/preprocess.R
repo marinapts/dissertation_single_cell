2e02422ba4224a23dafae21e50fbcd1782b77078
marinapts <potsimarina@gmail.com>
2020-05-26 18:43:36 +0100

    Preprocess E13 and E14 hom datasets

--- preprocessing/preprocess.R
+++ preprocessing/preprocess.R
@@ -1,39 +1,51 @@
 library(dplyr)
 library(Seurat)
 library(patchwork)
+# require(ggplot2)
+
+load_dataset <- function(file, name) {
+    gene_expression_matrix <- read.csv(file, row.names=1)  # Load csv file
+    seuratObj <- CreateSeuratObject(counts=gene_expression_matrix, project=name)  # Create Seurat object
+    print(cat(name, dim(seuratObj)))
+    return(seuratObj)
+}
+
+qc_plots <- function(data, name) {
+    dev.off()
+    # Add percentage of mitochondrial genes to the Seurat object
+    data[["percent.mito"]] <- PercentageFeatureSet(data, pattern = "^MT-")
+    # Visualize QC metrics as a violin plot
+    violin_plot <- VlnPlot(data, features = c('nFeature_RNA', 'nCount_RNA', 'percent.mito'), ncol = 3)
+
+    # Visualize relationship between nCount_RNA and nFeature_RNA
+    scatter <- FeatureScatter(data, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+
+    # Remove values from the datasets
+    # data_subset <- subset(data, subset=nFeature_RNA < 7500 & nCount_RNA < 50000)
+}
 
 data_dir <- file.path(dirname(getwd()), 'data')
-E14_het_file = file.path(data_dir, 'E14_het.csv')
+print(data_dir)
 E14_hom_file = file.path(data_dir, 'E14_hom.csv')
+E13_hom_file = file.path(data_dir, 'E13_hom.csv')
 
-# Load the dataset
-E14_het_matrix <- read.csv(E14_het_file, row.names=1)
-E14_hom_matrix <- read.csv(E14_hom_file, row.names=1)
-print(cat('E14 het:', dim(E14_het_matrix)))
-print(cat('E14 hom:', dim(E14_hom_matrix)))
+# E14_HOM
+E14_hom <- load_dataset(E14_hom_file, 'E14_hom')
+qc_plots(E14_hom, 'E14_hom')
+E14_hom <- subset(E14_hom, subset=nFeature_RNA < 7500 & nCount_RNA < 50000)  # Filter out cells found from quality control
+print(cat('E14_hom subset: ', dim(E14_hom)))
+# LogNormalize normalizes the feature expression measurements for each cell by the total expression,
+# multiplies this by a scale factor (10,000 by default), and log-transforms the result.
+E14_hom <- NormalizeData(E14_hom, normalization.method = "LogNormalize", scale.factor = 10000)  # Normalise data
 
-# Create Seurat object for each dataset
-E14_het <- CreateSeuratObject(counts=E14_het_matrix, project="E14_het", min.cells=3, min.features=200)
-E14_hom <- CreateSeuratObject(counts=E14_hom_matrix, project="E14_hom", min.cells=3, min.features=200)
 
-# Merge into one single seurat object. Add cell ids in case you have overlapping barcodes between the datasets
-alldata <- merge(E14_het, E14_hom, add.cell.ids=c('E14_het', 'E14_hom'))
-table(Idents(alldata))  # Checks number of cells per each sample
+# E13_HOM
+E13_hom <- load_dataset(E13_hom_file, 'E13_hom')
+qc_plots(E13_hom, 'E13_hom')
+E13_hom <- subset(E13_hom, subset=nFeature_RNA < 6000 & nCount_RNA < 35000)  # Filter out cells found from quality control
+print(cat('E13_hom subset: ', dim(E13_hom)))
 
-# Add percentage of mitochondrial genes
-E14_het[["percent.mito"]] <- PercentageFeatureSet(E14_het, pattern = "^MT-")
-alldata[["percent.mito"]] <- PercentageFeatureSet(alldata, pattern = "^MT-")
-# Show QC metrics for the first 5 cells
-head(alldata@meta.data, 10)
 
-VlnPlot(E14_het, features = c('nFeature_RNA', 'nCount_RNA', 'percent.mito'), ncol = 3)
-VlnPlot(alldata, features = c('nFeature_RNA', 'nCount_RNA', 'percent.mito'), ncol = 3)
-# Visualize relationship between nCount_RNA and nFeature_RNA
-FeatureScatter(E14_het, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-FeatureScatter(alldata, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-
-# Remove values from the datasets
-E14_het <- subset(E14_het, subset=nFeature_RNA > 6500 & nCount_RNA > 40000)
-E14_hom <- subset(E14_hom, subset=nFeature_RNA > 7500 & nCount_RNA > 45000)
-print(cat('E14 het:', dim(E14_het)))
-print(cat('E14 hom:', dim(E14_hom)))
+# Merge E14_hom and E13_hom only for visualisation purposes - avoid 2 plots
+E13_E14 <- merge(E13_hom, E14_hom, add.cell.ids=c('E13_hom', 'E14_hom'))
+scatter <- FeatureScatter(E13_E14, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
