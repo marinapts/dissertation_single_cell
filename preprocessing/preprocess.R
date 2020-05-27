e229bbaa0d9615fdd62815f90175396234a8e0ef
marinapts <potsimarina@gmail.com>
2020-05-27 02:54:14 +0100

    Normalise data

--- preprocessing/preprocess.R
+++ preprocessing/preprocess.R
@@ -11,28 +11,28 @@ load_dataset <- function(file, name) {
 }
 
 qc_plots <- function(data, name) {
-    dev.off()
     # Add percentage of mitochondrial genes to the Seurat object
     data[["percent.mito"]] <- PercentageFeatureSet(data, pattern = "^MT-")
     # Visualize QC metrics as a violin plot
     violin_plot <- VlnPlot(data, features = c('nFeature_RNA', 'nCount_RNA', 'percent.mito'), ncol = 3)
-
     # Visualize relationship between nCount_RNA and nFeature_RNA
-    scatter <- FeatureScatter(data, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
-
-    # Remove values from the datasets
-    # data_subset <- subset(data, subset=nFeature_RNA < 7500 & nCount_RNA < 50000)
+    scatter_plot <- FeatureScatter(data, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+    return(list(violin_plot, scatter_plot))
+    # while (!is.null(dev.list()))  dev.off()
 }
 
+
+# Load datasets
 data_dir <- file.path(dirname(getwd()), 'data')
 print(data_dir)
 E14_hom_file = file.path(data_dir, 'E14_hom.csv')
 E13_hom_file = file.path(data_dir, 'E13_hom.csv')
 
+# Quality Control and Normalisation
 # E14_HOM
 E14_hom <- load_dataset(E14_hom_file, 'E14_hom')
 qc_plots(E14_hom, 'E14_hom')
-E14_hom <- subset(E14_hom, subset=nFeature_RNA < 7500 & nCount_RNA < 50000)  # Filter out cells found from quality control
+E14_hom <- subset(E14_hom, subset=nFeature_RNA < 7500 & nCount_RNA < 45000)  # Filter out cells found from quality control
 print(cat('E14_hom subset: ', dim(E14_hom)))
 # LogNormalize normalizes the feature expression measurements for each cell by the total expression,
 # multiplies this by a scale factor (10,000 by default), and log-transforms the result.
@@ -44,8 +44,9 @@ E13_hom <- load_dataset(E13_hom_file, 'E13_hom')
 qc_plots(E13_hom, 'E13_hom')
 E13_hom <- subset(E13_hom, subset=nFeature_RNA < 6000 & nCount_RNA < 35000)  # Filter out cells found from quality control
 print(cat('E13_hom subset: ', dim(E13_hom)))
-
+E13_hom <- NormalizeData(E13_hom, normalization.method = "LogNormalize", scale.factor = 10000)  # Normalise data
 
 # Merge E14_hom and E13_hom only for visualisation purposes - avoid 2 plots
 E13_E14 <- merge(E13_hom, E14_hom, add.cell.ids=c('E13_hom', 'E14_hom'))
-scatter <- FeatureScatter(E13_E14, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
+pdf(file="feature_scatter_plot.pdf")
+FeatureScatter(E13_E14, feature1 = "nCount_RNA", feature2 = "nFeature_RNA")
