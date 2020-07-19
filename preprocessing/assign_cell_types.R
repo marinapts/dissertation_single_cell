63c739c4e7557b9becdcd9ec309d9bcfcf17ac40
marinapts <potsimarina@gmail.com>
2020-07-19 07:24:45 +0100

    Create cell type annotations for E14_hom_all_genes using cellassign

---
+++ preprocessing/assign_cell_types.R
@@ -0,0 +1,57 @@
+library(Seurat)
+library(tensorflow)
+library(cellassign)
+library(pheatmap)
+library(here)
+library(scater)
+library(rhdf5)
+library(hdf5r)
+library(SeuratDisk)
+library(scran)
+
+set.seed(1234)                # set R rng seed
+reticulate::py_set_seed(1234) # set python rng seed
+
+data_dir <- here('ann_data/exp_04')
+
+h5ad_file <- file.path(data_dir, 'E14_hom_norm_all_genes.h5ad')
+Convert(h5ad_file, dest = 'h5seurat', overwrite = TRUE)
+E14_hom <- LoadH5Seurat(file.path(data_dir, 'E14_hom_norm_all_genes.h5seurat'))
+E14_hom <- as.SingleCellExperiment(E14_hom, assay = 'RNA')  # Convert to SingleCellExperiment class
+E14_hom <- computeSumFactors(E14_hom)
+sfactors <- sizeFactors(E14_hom)
+
+marker_list <- list(
+    `Neural progenitors` = c('Pax6', 'Vim', 'Sox2'),
+    `Intermediate progenitors` = c('Eomes', 'Btg2'),
+    `Post-mitotic neurons` = c('Tbr1', 'Sox5'),
+    `Ectopic cells` = c('Gsx2', 'Prdm13', 'Dlx1', 'Dlx2', 'Dlx5', 'Gad1', 'Gad2', 'Ptf1a', 'Msx3', 'Helt', 'Olig3')
+    # `Ectopic cells` = c('Prdm13', 'Dlx1', 'Dlx2', 'Dlx5', 'Msx3')
+)
+
+# Construct binary gene-by-celltype marker gene matrix
+mgi <- marker_list_to_mat(marker_list, include_other = FALSE)
+pheatmap(mgi)
+
+
+# Run cellassign
+fit_E14_hom <- cellassign(exprs_obj = E14_hom[rownames(mgi),],
+                          marker_gene_info = mgi,
+                          s = sfactors,
+                          # s = colSums(SummarizedExperiment::assay(E14_hom, "counts")),
+                          learning_rate = 1e-2,
+                          shrinkage = TRUE,
+                          verbose = TRUE,
+                          num_runs = 10)
+
+E14_hom$cell_type <- fit_E14_hom$cell_type
+plotUMAP(E14_hom, colour_by = 'cell_type')
+
+# Now that we have the cell types in the SingleCellExperiment object, we convert back to Seurat because it's easier
+# to store the object in an h5ad file
+E14_hom_seurat <- as.Seurat(E14_hom)
+write.csv(x = E14_hom_seurat$cell_type, file = file.path(data_dir, 'E14_hom_cellassign.csv'), row.names = TRUE)
+
+
+
+
